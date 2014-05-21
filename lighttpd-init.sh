01d102d1e47021a01a035ca5ad54267856b0fa86
geotom <tarnold@walkercivil.com>
2014-05-21 12:42:52 -0500

    Removed logging and temp setup

--- lighttpd-init.sh
+++ lighttpd-init.sh
@@ -6,35 +6,19 @@
 # This is a guide for setting up Lighttpd on an Amazon Linux instance.
 # Think of it as loose documentation that may easily be turned into a script.
 # 
-# This is the log file:
-LOGFILE="log.lighttpd-init.txt"
-echo "--> lighttpd-init.sh --> Beginning initialisation script." | tee $LOGFILE
+echo "--> lighttpd-init.sh --> Beginning initialisation script."
 # Begin by updating the system -- all yum packages
 echo "--> Run Yum updates? (answer 'y' to run)"
 read PROCEED
 if [ "$PROCEED" == 'y' ]; then
-  echo "--> Updating using yum update" | tee -a $LOGFILE
-  sudo yum -y update | tee -a $LOGFILE
-  echo "-->Yum updates complete." | tee -a $LOGFILE
+  echo "--> Updating using yum update"
+  sudo yum -y update
+  echo "-->Yum updates complete."
 else
-  echo "--> Skipping yum updates" | tee -a $LOGFILE
+  echo "--> Skipping yum updates"
 fi
 # Install lighttpd
-sudo yum -y install lighttpd-fastcgi | tee -a $LOGFILE
-echo "--> Configuring Lighttpd" | tee -a $LOGFILE
-LIGHTYDIR="./lighttpd"
-LIGHTYCONF=$LIGHTYDIR"/port3033.conf"
-LIGHTYROOT="/var/www/lighttpd"
-mkdir $LIGHTYDIR
-#TODO the following works except for literally putting \> in the file
-echo server.document-root = \"$LIGHTYROOT\" > $LIGHTYCONF
-echo '
-server.port = 3033
-
-mimetype.assign = (
-  ".html" => "text/html", 
-  ".txt" => "text/plain",
-  ".jpg" => "image/jpeg",
-  ".png" => "image/png" 
-)' >> $LIGHTYCONF
-
+sudo yum -y install lighttpd-fastcgi
+# TODO: add configuration notes on how to
+# proxy geoserver and django static files
+# also need fastcgi notes for Django
